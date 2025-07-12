3514708d3de83a3e943d00deda735ae5e257e7f1
alexu <alex.unapel@gmail.com>
2025-07-11 23:50:05 -0300

    trabalhando no menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppLayoutComponent } from './layout/app-layout/app-layout.component';
 import { ListarCategoriaComponent } from './features/categoria/listar-categoria/listar-categoria.component';
+import { ManterCategoriaComponent } from './features/categoria/manter-categoria/manter-categoria.component';
 
 const routes: Routes = [
-  {path: '', component: AppLayoutComponent, 
+  {
+    path: '',
+    component: AppLayoutComponent,
     children: [
-  {path: 'categoria', component: ListarCategoriaComponent},
+      { path: 'categoria', component: ListarCategoriaComponent },
+      { path: 'manterCategoria', component: ManterCategoriaComponent },
     ],
   },
   { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/layout/app-layout/app-layout.component.ts
+++ src/app/layout/app-layout/app-layout.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faHeadphones } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-app-layout',
   templateUrl: './app-layout.component.html',
   styleUrls: ['./app-layout.component.css']
 })
-export class AppLayoutComponent {
+export class AppLayoutComponent implements OnInit {
+ faHeadphones = faHeadphones
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
