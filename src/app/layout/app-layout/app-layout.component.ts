fd3ea438966a5cd3aa977483ea872fcb2eb00c98
alexu <alex.unapel@gmail.com>
2025-07-14 22:42:51 -0300

    ajustes na sidebar

--- src/app/layout/app-layout/app-layout.component.ts
+++ src/app/layout/app-layout/app-layout.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { faBars, faHeadphones } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBars,
+  faHeadphones,
+  faUserTie,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-app-layout',
   templateUrl: './app-layout.component.html',
-  styleUrls: ['./app-layout.component.css']
+  styleUrls: ['./app-layout.component.css'],
 })
 export class AppLayoutComponent implements OnInit {
- faHeadphones = faHeadphones;
- fabars = faBars;
+  faHeadphones = faHeadphones;
+  fabars = faBars;
+  faUserTie = faUserTie;
+
+  showSidebar: boolean = true;
 
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
 
+   toggleSidebar(): void {
+    this.showSidebar = !this.showSidebar;
+  }
 }
